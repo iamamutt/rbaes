bb076b4ea9c8cd6bfaea80d55145bb040d8d86c9
Joseph M. Burling <josephburling@gmail.com>
2018-04-26 23:54:36 -0700

    Delete power.R

--- .junk/vignettes/power.R
+++
@@ -1,6 +0,0 @@
-## ---- echo=FALSE, results='hide', message=FALSE--------------------------
-knitr::opts_chunk$set(eval = FALSE, comment = "", message = FALSE)
-library(rbaes)
-
-## ------------------------------------------------------------------------
-# stanfit <- rbaes::example_stan()
